1004abb1c92da3b63917a1057820d336e6d06fca
debashish999 <debashishpradhan29@gmail.com>
2021-07-08 13:49:49 +0530

    first push

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+
+
+
+
+
+
+@include('pages.nav')
+
+
+
+@section('content')
+    <h1><?php echo $title; ?></h1>
+ 
+    <p>this is about us page</p>
+    
+
+
+
+@endsection
---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+
+
+
+@section('content')
+@include('pages.nav')
+    <h1>{{ $title }}</h1>
+   
+
+    @if(count($services) > 0)
+    <ul>
+        @foreach($services as $service)
+        <li>{{ $service }}</li>
+
+        @endforeach
+        </ul>
+
+
+    @endif
+
+
+
+    <p>This is laravel Services</p>
+
+@endsection
